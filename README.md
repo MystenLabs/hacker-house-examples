ef7628f3d494f76f2169f55fa11411c0e36edf69
Jian Lu <123408603+healthydeve@users.noreply.github.com>
2023-10-12 10:21:59 -0400

    examples for more modern usage

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 ~/sui/target/debug/sui client publish --gas-budget 2000000000 sources
 
 Now we get 10 million Gold, and 1 million Fish, Iron, Wood
+
+# Transfer example
+
+Fish
+0xe14891fe2df8da5ca23d8c72a2047ff80c342668337009d6e924e31e5f678754
+Gold
+0xaeb77d1785ec0c81e24810533fdca7477c7dae36306efa6b2c7bd8bbe9292cdd
+
+sui client transfer --to 0xd02c0676a9cd9d70e554b5c3ee8bd17cfe93792f6aaa327b02b096ac3359a210 --object-id 0xe14891fe2df8da5ca23d8c72a2047ff80c342668337009d6e924e31e5f678754 --gas-budget 1000000000
+
+sui client transfer --to 0xd02c0676a9cd9d70e554b5c3ee8bd17cfe93792f6aaa327b02b096ac3359a210 --object-id 0xaeb77d1785ec0c81e24810533fdca7477c7dae36306efa6b2c7bd8bbe9292cdd --gas-budget 1000000000
--- deepbook-example.mjs
+++ deepbook-example.mjs
@@ -34,10 +34,10 @@ export async function executeTransactionBlock(
 export const FLOAT_SCALING_FACTOR = 1_000_000_000n;
 export const DEFAULT_TICK_SIZE = 1n * FLOAT_SCALING_FACTOR;
 export const DEFAULT_LOT_SIZE = 1n;
-export const GOLD = '0x702e0c66dfeaf9fc37d9b372f399163985dbd40cf7972bda7ba2caadfd256575::gold::GOLD';
-export const FISH = '0x702e0c66dfeaf9fc37d9b372f399163985dbd40cf7972bda7ba2caadfd256575::fish::FISH';
-export const IRON = '0x702e0c66dfeaf9fc37d9b372f399163985dbd40cf7972bda7ba2caadfd256575::iron::IRON';
-export const WOOD = '0x702e0c66dfeaf9fc37d9b372f399163985dbd40cf7972bda7ba2caadfd256575::wood::WOOD';
+export const GOLD = '0x624a1cf629d8af49987ee59ba5d2f5fcc0495f212cc6058e4b4c353f5ae0f2f5::gold::GOLD';
+export const FISH = '0x624a1cf629d8af49987ee59ba5d2f5fcc0495f212cc6058e4b4c353f5ae0f2f5::fish::FISH';
+export const IRON = '0x624a1cf629d8af49987ee59ba5d2f5fcc0495f212cc6058e4b4c353f5ae0f2f5::iron::IRON';
+export const WOOD = '0x624a1cf629d8af49987ee59ba5d2f5fcc0495f212cc6058e4b4c353f5ae0f2f5::wood::WOOD';
 // coin::Coin<0xf398b9ecb31aed96c345538fb59ca5a1a2c247c5e60087411ead6c637129f1c4::gold::GOLD>
 // coin::Coin<0xf398b9ecb31aed96c345538fb59ca5a1a2c247c5e60087411ead6c637129f1c4::fish::FISH>
 // coin::Coin<0xf398b9ecb31aed96c345538fb59ca5a1a2c247c5e60087411ead6c637129f1c4::iron::IRON>
@@ -51,33 +51,34 @@ const deepbook_client = new DeepBookClient(local_client);
 // Create pools for gold / fish
 
 // pool_id: 0x538091bde22b3e38aae569ed1fb8621714c8193bc6819ea2e5ebb9ae700a42d2
-// const txb_fish = deepbook_client.createPool(GOLD, FISH, DEFAULT_TICK_SIZE, DEFAULT_LOT_SIZE);
+const txb_fish = deepbook_client.createPool(GOLD, FISH, DEFAULT_TICK_SIZE, DEFAULT_LOT_SIZE);
 
-// const fish = await local_client.signAndExecuteTransactionBlock({transactionBlock: txb_fish,signer: keypair});
+const fish = await local_client.signAndExecuteTransactionBlock({transactionBlock: txb_fish,signer: keypair});
 // console.log(fish);
 // We can use this to make a game 
 // Create order examples after initializing a custodian cap.
 // const create_account_tx = deepbook_client.createAccount('0x255592ec5a35592aadf16f50aa9867ea92a5ea2866492d3fc6dd767596417dae');
 // const res = await local_client.signAndExecuteTransactionBlock({transactionBlock: create_account_tx, signer: keypair});
-
+// console.log(res);
 // const res = await executeTransactionBlock(rawSigner, create_account_tx);
 // console.log(res);
 // Account_cap: 0x3b221136175c956ad551c3f9d874e87f65f1221c47077a01c0b6eef7f910580b
 // 0x702e0c66dfeaf9fc37d9b372f399163985dbd40cf7972bda7ba2caadfd256575
-const fish_pool_id = '0x43db437be5186cf75bbbf1fdc78019aa884aad5dcd8822c2195a79f35abc261d';
+const fish_pool_id = '0xa38de8cc4366d8e08e7a894c92664b45372ccee836c78f2fd1f30dbc53adbcc4';
 // const test = await deepbook_client.getPoolInfo(fish_pool_id);
 // console.log(test);
 
-deepbook_client.setAccountCap('0xcaba104a504d92efad33fb9f7db27cbf8bf765363224c899b8c802e713667f10');
+deepbook_client.setAccountCap('0xdc6f5964c430aba49964427047a0ed0964a9e51c6649421b0583411a727125c1');
+// deepbook_client.setAccountCap('0xd212c0ead5ddcaf3afe044f816901f36c2a5bab147d263d07d319dc50af4850b'); // accoutn cap 2 to test
 // // Deposit some base asset into a pool 
-const fish_coin_id = '0xa08d69d30b64b2ad65ea914a92e94a9808a6f0217c9d75e587ff6dcee18b0a9d';
+const fish_coin_id = '0xb4da3a3bec7f1cee80cf00e81d91f566c2ffa085df30b3b16788eb09114a4ba1';
 
-// const fish_deposit_txn = await deepbook_client.deposit(fish_pool_id, fish_coin_id, BigInt(1000000000));
+// const fish_deposit_txn = await deepbook_client.deposit(fish_pool_id, fish_coin_id, BigInt(10000000000));
 // const fish_deposit = await local_client.signAndExecuteTransactionBlock({transactionBlock: fish_deposit_txn, signer: keypair});
 
 // // const fish_deposit = await executeTransactionBlock(rawSigner, fish_deposit_txn);
 
-const gold_coin_id = '0xfa9c8a2c0528e5be2255529304e6b42c4f448556c3b9efc7ec221bedf94b1f83';
+const gold_coin_id = '0xa92ba4377f414d667911df3ecbb6524018b7b7f46c73df2a539181e654154dfd';
 // const gold_deposit_txn = await deepbook_client.deposit(fish_pool_id, gold_coin_id, BigInt(100000000000));
 // const gold_deposit = await local_client.signAndExecuteTransactionBlock({transactionBlock: gold_deposit_txn, signer: keypair});
 
@@ -85,31 +86,32 @@ const gold_coin_id = '0xfa9c8a2c0528e5be2255529304e6b42c4f448556c3b9efc7ec221bed
 
 // // // console.log(fish_deposit);
 // // // Deposit quote asset into this pool
-// const limit_order_txn_ask = await deepbook_client.placeLimitOrder(fish_pool_id, 2n* DEFAULT_TICK_SIZE, 1n, "ask");
+const limit_order_txn_ask = await deepbook_client.placeLimitOrder(fish_pool_id, 2n* DEFAULT_TICK_SIZE, 100000n, "ask");
+const limit_ask = await local_client.signAndExecuteTransactionBlock({transactionBlock: limit_order_txn_ask, signer: keypair});
+
+// const limit_order_txn_ask = await deepbook_client.placeLimitOrder(fish_pool_id, 1n* DEFAULT_TICK_SIZE, 100n, "bid");
 // const limit_ask = await local_client.signAndExecuteTransactionBlock({transactionBlock: limit_order_txn_ask, signer: keypair});
 
 // // const limit_order_ask = await executeTransactionBlock(rawSigner, limit_order_txn_ask);
 
-// const limit_order_txn_bid = await deepbook_client.placeLimitOrder(fish_pool_id, 1n* DEFAULT_TICK_SIZE, 1n, "bid");
+// const limit_order_txn_bid = await deepbook_client.placeLimitOrder(fish_pool_id, 1n* DEFAULT_TICK_SIZE, 100000n, "bid");
 // const limit_bid = await local_client.signAndExecuteTransactionBlock({transactionBlock: limit_order_txn_bid, signer: keypair});
-
-// const limit_order_bid = await executeTransactionBlock(rawSigner, limit_order_txn_bid);
 
 // // console.log(limit_order_ask);
 // // console.log(limit_order_bid);
 
-const resp = await local_client.getAllCoins({
-  owner: '0x255592ec5a35592aadf16f50aa9867ea92a5ea2866492d3fc6dd767596417dae',
-  // coinType: '0x86c94199a8f53097b5cdcadf2559454499f23774795c1768917705ef4b004b56::gold::GOLD',
-});
+// const resp = await local_client.getAllCoins({
+//   owner: '0x255592ec5a35592aadf16f50aa9867ea92a5ea2866492d3fc6dd767596417dae',
+//   // coinType: '0x86c94199a8f53097b5cdcadf2559454499f23774795c1768917705ef4b004b56::gold::GOLD',
+// });
 // console.log(resp);
 
-const base = resp.data[2].coinObjectId;
-// console.log(baseCoin);
-const place_market_order = await deepbook_client.placeMarketOrder(fish_pool_id, BigInt(1), "ask", base, undefined, undefined,'0x255592ec5a35592aadf16f50aa9867ea92a5ea2866492d3fc6dd767596417dae');
-const limit_bid = await local_client.signAndExecuteTransactionBlock({transactionBlock: place_market_order, signer: keypair});
+// const base = resp.data[3].coinObjectId;
+// // console.log(baseCoin);
+// const place_market_order = await deepbook_client.placeMarketOrder(fish_pool_id, BigInt(100), "ask", base, undefined, undefined,'0x255592ec5a35592aadf16f50aa9867ea92a5ea2866492d3fc6dd767596417dae');
+// const limit_bid = await local_client.signAndExecuteTransactionBlock({transactionBlock: place_market_order, signer: keypair});
 
-console.log(limit_bid)
+// console.log(limit_bid)
 
 // const get_asks = await deepbook_client.getLevel2BookStatus(fish_pool_id, BigInt(0), 10n *DEFAULT_TICK_SIZE, 'ask');
 // console.log(get_asks);
